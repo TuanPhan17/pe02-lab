b137e89e7293b54830ad1ce254a92da6a6deb087
TuanPhan17 <t_phan17@yahoo.com>
2025-10-16 17:17:37 -0700

    Finalized pe02.py with all 5 programs completed

--- pe02.py
+++ pe02.py
@@ -62,7 +62,7 @@ elif choice == 2:
         second = 1  #Start w/ first two numbers in sequence 
         print(first)
         print(second)
-        
+
         # USING LOOP TO GENERATE REST 
         count = 2 # already printed 2 numbers 
         while count < number:  # keep looping until we've generated the number of Fibonacci numbers the user asked for
@@ -90,4 +90,55 @@ elif choice == 3:
     else:  # If the person is age 65 or older
         print("Person is an elder.")
 
+
+elif choice == 4:
+    # --- Movie Ticket Pricing ---
+    
+    while True:  # USING LOOP to ask for age repeatedly 
+        age_input = input("Enter the person's age (or 'q' to quit): ")
+        if age_input.lower() == 'q':
+            break
+
+        person_age = int(age_input)
+
+        if person_age < 3:  # If a person is under the age of 3
+            print("Ticket is free.")
+        elif person_age <= 12:  # If they are between 3 and 12
+            print("Ticket is $10.")
+        else:  # If they are over age 12
+            print("Ticket is $15.")
+
+
+elif choice == 5:
+    #--- Patern printing code---
+
+#PSEUDOCODE PATTERN PRINT 
+#Ask user how many rows they want in pattern
+# Example: if they say 4, we'll have 4 lines of stars 
+# Start a loop that goes from 1 up to the number of rows 
+#Each time loop runs , one row of stars get printed 
+#inside loop , Figure out how many stars in loop, same as loop number 
+#print that many stars * using repetion times loop number 
+#Loop finished pattern is done 
+
+    total_rows = int(input("Enter how many rows you want in the pattern: ")) 
+
+    current_row = 1 # starting with first row 
+
+    while current_row <= total_rows:  # While current row is less than or equal to total rows 
+
+        stars = "" # We will make an empty string to keep the stars in
+
+        count = 1
+        while count <= current_row: # using inner loop to build pattern adding stars 
+            stars = stars + "*"
+            count = count + 1
+
+        print(stars) # Once all stars are added, print the stars 
+
+        current_row = current_row + 1 # Move to the next row
+
+
+else:
+    print("Invalid choice! Please enter 1-5.")
 
